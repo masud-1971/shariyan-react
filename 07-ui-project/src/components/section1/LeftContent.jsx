c311549c7de0eecfdce153f9e996d977761b8bea
Masud Rana <msd.rana.2008@gmail.com>
2025-11-06 21:04:34 +0600

    page-1 left section done

--- 07-ui-project/src/components/section1/LeftContent.jsx
+++ 07-ui-project/src/components/section1/LeftContent.jsx
@@ -3,8 +3,21 @@ import "remixicon/fonts/remixicon.css";
 
 const LeftContent = () => {
   return (
-    <div className="h-full w-1/4 bg-blue-500">
-      <div></div>
+    <div className="h-full w-1/4 bg-gray-200 flex flex-col justify-between rounded-2xl">
+      <div className="p-5">
+        <h2 className="text-4xl font-bold">
+          Prospective <br />{" "}
+          <span className="text-gray-400">
+            customer
+          </span>{" "}
+          <br /> segmentation
+        </h2>
+        <p className="mt-10 text-lg font-medium text-gray-600">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo
+          eligendi esse sed accusantium dolorum nam exercitationem pariatur
+          praesentium temporibus quos.
+        </p>
+      </div>
       <div className="text-8xl">
         <i class="ri-arrow-right-up-line"></i>
       </div>
