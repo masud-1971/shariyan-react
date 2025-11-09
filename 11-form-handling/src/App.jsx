fad7023c94f43f6b7544a706b5609d0e7ccd6018
Masud Rana <msd.rana.2008@gmail.com>
2025-11-09 21:12:25 +0600

    Basic form handling

---
+++ 11-form-handling/src/App.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const App = () => {
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  };
+
+  return (
+    <div>
+      <h1 className="text-8xl">Form</h1>
+      <form
+        onSubmit={(e) => {
+          submitHandler(e);
+        }}
+      >
+        <input
+          className="border-amber-400 border-4 p-3 m-2 text-[20px]"
+          type="text"
+          placeholder="Enter your name"
+        />
+        <input
+          className="border-amber-400 border-4 p-3 m-2 text-[20px]"
+          type="text"
+          placeholder="Enter your email"
+        />
+        <div className="">
+          <button
+            className="bg-amber-500 text-white text-5xl p-3 m-2 rounded-lg"
+            type="submit"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default App;
